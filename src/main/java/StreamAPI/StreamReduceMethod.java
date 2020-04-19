797a269641aac3be35b40c94eed3b17fb7fedbf7
Saurabh <saurabh.verma.miet@gmail.com>
2020-04-19 18:16:57 +0530

    Stream reduce method

--- src/main/java/StreamAPI/StreamReduceMethod.java
+++ src/main/java/StreamAPI/StreamReduceMethod.java
@@ -1,4 +1,28 @@
 package StreamAPI;
 
+import java.util.*;
+
 public class StreamReduceMethod {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 2; i < 20; i++) {
+            numbers.add(i);
+        }
+
+        Optional<Integer> output = numbers.stream().reduce((a, b) -> a > b ? a : b);
+        output.ifPresent(System.out::println);
+
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sum);
+
+
+        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        sum = Arrays.stream(num).reduce(0, (a, b) -> a + b);
+
+        System.out.println(sum);
+
+    }
 }
