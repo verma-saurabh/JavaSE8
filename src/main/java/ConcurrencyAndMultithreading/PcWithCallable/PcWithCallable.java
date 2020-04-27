c5f3b506f64030bb7f6a96b01fd8c84d9b804eef
Saurabh <saurabh.verma.miet@gmail.com>
2020-04-27 20:08:20 +0530

    Producer Consumer with callable

---
+++ src/main/java/ConcurrencyAndMultithreading/PcWithCallable/PcWithCallable.java
@@ -0,0 +1,109 @@
+package ConcurrencyAndMultithreading.PcWithCallable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PcWithCallable {
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Integer> buffer = new ArrayList<>();
+        Lock lock = new ReentrantLock();
+        Condition isEmpty = lock.newCondition();
+        Condition isFull = lock.newCondition();
+
+
+        class Producer implements Callable<String> {
+
+            @Override
+            public String call() throws Exception {
+                int count = 0;
+
+                while (count++ < 50) {
+                    try {
+                        lock.lock();
+                        while (isFull(buffer)) {
+                            isFull.await();
+                        }
+                        buffer.add(1);
+                        isEmpty.signalAll();
+                    } finally {
+                        lock.unlock();
+                    }
+
+                }
+
+                return "Produced " + (count - 1);
+            }
+        }
+
+        class Consumer implements Callable<String> {
+
+            @Override
+            public String call() throws Exception {
+                int count = 0;
+                while (count++ < 50) {
+                    try {
+
+                        lock.lock();
+                        while (isEmpty(buffer)) {
+                            isEmpty.await();
+                        }
+                        buffer.remove(buffer.size() - 1);
+                        isFull.signalAll();
+                    } finally {
+                        lock.unlock();
+                    }
+
+                }
+                return "Consumed " + (count - 1);
+            }
+        }
+
+        List<Producer> producers = new ArrayList<>();
+        List<Consumer> consumers = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            producers.add(new Producer());
+            consumers.add(new Consumer());
+        }
+
+
+        List<Callable<String>> procucersconsumers = new ArrayList<>();
+        procucersconsumers.addAll(producers);
+        procucersconsumers.addAll(consumers);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
+        try {
+            List<Future<String>> futures = executorService.invokeAll(procucersconsumers);
+
+            futures.forEach(future -> {
+                try {
+                    System.out.println(future.get());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+        } finally {
+            executorService.shutdown();
+            System.out.println("Executor Service Shutdown");
+        }
+
+    }
+
+    static boolean isFull(List<Integer> buffer) {
+        return buffer.size() > 50;
+    }
+
+    static boolean isEmpty(List<Integer> buffer) {
+        return buffer.size() == 0;
+    }
+}
