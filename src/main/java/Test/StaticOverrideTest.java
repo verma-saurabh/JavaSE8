eb7465a2b8446e68019c5f3ee7e02112403d95de
Saurabh <saurabh.verma.miet@gmail.com>
2020-07-02 16:45:14 +0530

    static override test

--- src/main/java/Test/StaticOverrideTest.java
+++ src/main/java/Test/StaticOverrideTest.java
@@ -1,4 +1,27 @@
 package Test;
 
 public class StaticOverrideTest {
+    public static void main(String[] args) {
+        A a = new A();
+        a.show();
+        A b = new B();
+        b.show();
+        B c = new B();
+        c.show();
+
+
+    }
+}
+
+class A {
+    public static void show() {
+        System.out.println("Inside A");
+    }
+
+}
+
+class B extends A {
+    public static void show() {
+        System.out.println("Inside B");
+    }
 }
