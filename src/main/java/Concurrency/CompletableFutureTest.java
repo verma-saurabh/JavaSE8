1439bb6618a24c20687c6da05c8264ccf5fca20a
Saurabh <saurabh.verma.miet@gmail.com>
2020-07-06 21:51:55 +0530

    completable future test

--- src/main/java/Concurrency/CompletableFutureTest.java
+++ src/main/java/Concurrency/CompletableFutureTest.java
@@ -1,4 +1,14 @@
 package Concurrency;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
+
 public class CompletableFutureTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        CompletableFuture.runAsync(() -> System.out.println(Thread.currentThread().getName() + " I am running async"));
+        executorService.shutdown();
+    }
 }
