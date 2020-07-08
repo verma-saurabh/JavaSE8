5db3219324747e64c6868d768af79513c9b7ff99
Saurabh <saurabh.verma.miet@gmail.com>
2020-07-08 19:25:40 +0530

    completable future with supplier

--- src/main/java/Concurrency/CompletableFutureWithSupplier.java
+++ src/main/java/Concurrency/CompletableFutureWithSupplier.java
@@ -1,4 +1,30 @@
 package Concurrency;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
+
 public class CompletableFutureWithSupplier {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Supplier<String> supplier = () -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return Thread.currentThread().getName();
+        };
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(supplier, executor);
+
+        String string = completableFuture.join();
+        completableFuture.obtrudeValue("Too Long");
+        System.out.println(string);
+
+
+        string = completableFuture.join();
+        System.out.println(string);
+        executor.shutdown();
+    }
 }
