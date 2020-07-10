54443eac69a6f97e3f99460f03466198390837da
Saurabh <saurabh.verma.miet@gmail.com>
2020-07-10 19:54:09 +0530

    Forkjoin Malformed repository

--- src/main/java/Concurrency/repository/ForkJoinMalformedIdentityRepository.java
+++ src/main/java/Concurrency/repository/ForkJoinMalformedIdentityRepository.java
@@ -3,15 +3,26 @@ package Concurrency.repository;
 import Concurrency.Identity;
 
 import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ForkJoinMalformedIdentityRepository implements MalformedIdentityRepository {
+
+    private ExecutorService pool = Executors.newWorkStealingPool();
+
+    public ForkJoinMalformedIdentityRepository(MalformedIdentityRepository delegate) {
+        this.delegate = delegate;
+    }
+
+    private MalformedIdentityRepository delegate;
+
     @Override
     public void addIdentity(Identity identity, String reason) {
-
+        pool.submit(() -> delegate.addIdentity(identity, reason));
     }
 
     @Override
     public void addIdentity(InputStream message, String reason) {
-
+        pool.submit(() -> delegate.addIdentity(message, reason));
     }
 }
